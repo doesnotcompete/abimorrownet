3a36cddeab279b613e86d5686088d8ca320123d3
luxojr10 <kevin@quosh.net>
2015-04-12 21:34:57 +0200

    Fixed smaller bugs related to nominations.

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,6 +1,6 @@
 class Award < ActiveRecord::Base
   belongs_to :voting
-  has_many :nominations
+  has_many :nominations, dependent: :destroy
   has_many :users, through: :nominations
 
   validates :title, :tiers, :voting, presence: true
